f9cd461ee75c11018721d00023da00225a8ef305
STIXGT <stiveguano@gmail.com>
2024-05-24 03:09:22 -0500

    update

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const dialog = document.querySelector('#dialog')
+const buttonDialog = document.querySelector('.main-dialog')
+
+buttonDialog.addEventListener('click', () =>{
+    dialog.showModal()
+})
